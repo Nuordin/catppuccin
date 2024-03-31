574c09c2f12edf1a01bba9633adffb810b012926
Nuordin <nuordin_mohamed@proton.me>
2024-04-01 01:33:52 +0300

    deleiting old comminted stuf

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
--- Disable nvim-tree
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
